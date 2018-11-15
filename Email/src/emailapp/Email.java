e68a3fe06a4f85e64a0a720ce5b6fb318b1453e1
sriv-shubham <shubham.136@gmail.com>
2018-11-16 00:33:19 +0530

    initializations

--- Email/src/emailapp/Email.java
+++ Email/src/emailapp/Email.java
@@ -2,4 +2,22 @@ package emailapp;
 
 public class Email {
 
+	private String firstname;
+	private String lastname;
+	private String password;
+	private String department;
+	private int mailboxcapacity;
+	private String alternateEmail;
+	
+	//constructor to recieve first name and last name
+	
+	//Ask for the department
+	
+	//Generate a random password
+	
+	//set the mailbox capacity
+	
+	//set the alternate email
+	
+	//change the password
 }
